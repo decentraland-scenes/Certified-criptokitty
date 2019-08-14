76fe5b656634e8cdd504bcc8efb3bc4eb46432a4
nearnshaw <nearnshaw@decentraland.org>
2019-08-14 18:10:21 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Certified-criptokitty
+ Displays an NFT taken from a contract address
---
+++ src/game.ts
@@ -0,0 +1,31 @@
+
+// NFT picture frame
+const entity = new Entity()
+const shapeComponent = new NFTShape('ethereum://0x06012c8cf97BEaD5deAe237070F9587f8E7A266d/558536',Color3.Blue())
+entity.addComponent(shapeComponent)
+entity.addComponent(
+  new Transform({
+    position: new Vector3(4, 1.5, 4)
+  })
+)
+engine.addEntity(entity)
+
+
+// fixed wall
+const wall1 = new Entity()
+wall1.addComponent(new Transform({
+  position: new Vector3(4.5, 1, 4.10),
+  scale: new Vector3(4, 3, 0.05)
+}))
+wall1.addComponent(new BoxShape())
+engine.addEntity(wall1)
+
+
+// ground
+let floor = new Entity()
+floor.addComponent(new GLTFShape("models/FloorBaseGrass.glb"))
+floor.addComponent(new Transform({
+  position: new Vector3(8, 0, 8), 
+  scale:new Vector3(1.6, 0.1, 1.6)
+}))
+engine.addEntity(floor)
