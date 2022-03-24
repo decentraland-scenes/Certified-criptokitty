8f8e2e16b6154b4fec045862db258c15576cea15
nearnshaw <nearnshaw@decentraland.org>
2022-03-24 16:47:50 -0300

    lint fix and remove package-lock.json (if aplies)

--- src/game.ts
+++ src/game.ts
@@ -1,13 +1,13 @@
 // NFT picture frame
 const entity = new Entity()
 const shapeComponent = new NFTShape(
-  'ethereum://0x06012c8cf97BEaD5deAe237070F9587f8E7A266d/558536',
+  'ethereum://0x060tttt8cf97BEaD5deAe237070F9587f8E7A266d/558536',
   { color: Color3.Blue(), style: PictureFrameStyle.Gold_Carved }
 )
 entity.addComponent(shapeComponent)
 entity.addComponent(
   new Transform({
-    position: new Vector3(4, 1.5, 4),
+    position: new Vector3(4, 1.5, 4)
   })
 )
 engine.addEntity(entity)
@@ -17,19 +17,19 @@ const wall1 = new Entity()
 wall1.addComponent(
   new Transform({
     position: new Vector3(4.5, 1, 4.1),
-    scale: new Vector3(4, 3, 0.05),
+    scale: new Vector3(4, 3, 0.05)
   })
 )
 wall1.addComponent(new BoxShape())
 engine.addEntity(wall1)
 
 // ground
-let floor = new Entity()
+const floor = new Entity()
 floor.addComponent(new GLTFShape('models/FloorBaseGrass.glb'))
 floor.addComponent(
   new Transform({
     position: new Vector3(8, 0, 8),
-    scale: new Vector3(1.6, 0.1, 1.6),
+    scale: new Vector3(1.6, 0.1, 1.6)
   })
 )
 engine.addEntity(floor)
